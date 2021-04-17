39e7b2eb0a79f361640760ab418c09bc0cb51652
danidoble <dani.doble.d@hotmail.com>
2021-04-17 11:33:48 -0500

    bkc

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.setResourceRoot('/password_manager/public/');
+
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
